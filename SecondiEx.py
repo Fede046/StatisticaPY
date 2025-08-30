6721a7d1599e4d8092e01d5966aec3b62790c8c0
Federico <160421165+Fede046@users.noreply.github.com>
2025-08-30 19:28:45 +0200

    Update SecondiEx.py

--- SecondiEx.py
+++ SecondiEx.py
@@ -1,10 +1,27 @@
 
-
+#%%
 
 import scipy.stats as stats
-import numpy as np
-zalfamezzi = stats.norm.ppf(1-(1-0.95)/2)
-margine_di_errore = 1.5/np.sqrt(90)
-x_bar = 0.95
-interva = (x_bar-zalfamezzi*errore_marginale,x_bar+zalfamezzi*errore_marginale) 
+#La concentrazione di zuccheri in una bevanda non puo superare il 10%
+ #con una tolleranza del 5%. Il test di ipotesi su un campione di 70 bevande
+ #restutuisce un p-value p = 0.01. Posso certificare l’azienda
+
+
+#P value accettata H0 se maggiore di 0.05 altrimenti rifiuto -> rifiuto
+
+#%%
+
+import scipy.stats as stats
+
+#La concentrazione di zuccheri in una bevanda non puo superare il 10%.
+#Quale istruzione e’ corretta per fare un test di ipotesi su 70 bevande per
+#la certificazione?
+
+rvs = stats.uniform.rvs(size=70,random_state=10)
+tt = stats.ttest_1samp(rvs, popmean=0.1,alternative='greater')
+print(tt[1])
+
+
+
+
 
