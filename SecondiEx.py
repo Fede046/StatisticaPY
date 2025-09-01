e368ab8b02488b079b5c3bc4fab5ea6cdf9ca50a
Federico <160421165+Fede046@users.noreply.github.com>
2025-09-01 12:26:08 +0200

    cx

--- SecondiEx.py
+++ SecondiEx.py
@@ -1,25 +1,140 @@
+import scipy.stats as stats
+import numpy as np
+from scipy.optimize import minimize
+
+
+#Generare un campione casuale di 20 elementi da una distribuzione bino
+#miale con p=0.55, n=100, utilizzando come seme generatore 10. Stimare
+# con il metodo MLE il valore di p supponendo noto n.
+
+np.random.seed(10)
+
+campione = np.random.binomial(n=100, p=0.55,size=20)
+print(campione)
+
+#trovo il log della L(tetha)
+def log_likeH(theta,campione):
+    n = 100
+    p = theta
+    log_like = np.sum(stats.binom.logpmf(campione,n= n, p=p))
+    return log_like
+
+#Faccio il -log(L(Thetha)) -> Mimimizzo 
+#Minimizzazione della fuznione di verosmiglianza
+
+def neg_log_like(theta,campione):
+    return -log_likeH(theta, campione)
+
+#non dato da esercizio (inventato)
+#Ipotesi iniziale
+theta0 = 0.6
+
+res = minimize(neg_log_like,theta0,args=(campione,),method='Nelder-Mead')
+
+
+print(res.x[0])
 
 #%%
-
 import scipy.stats as stats
-#La concentrazione di zuccheri in una bevanda non puo superare il 10%
- #con una tolleranza del 5%. Il test di ipotesi su un campione di 70 bevande
- #restutuisce un p-value p = 0.01. Posso certificare l’azienda
+import numpy as np
+from scipy.optimize import minimize
 
 
-#P value accettata H0 se maggiore di 0.05 altrimenti rifiuto -> rifiuto
+#Generare un campione casuale di 30 elementi da una distribuzione normale
+# con media mu= 1 e deviazione standard std=1, utilizzando come seme
+# generatore 10. Stimare con il metodo MLE il valore di mu e std.
+
+np.random.seed(10)
+
+campione = np.random.normal(size=30,loc = 1,scale=1)
+
+def log_likeH(theta,campione):
+    mu,ds = theta
+    logL = np.sum(stats.norm.logpdf(campione,loc=mu,scale=ds))
+    return logL
+
+def neg_logL(theta,campione):
+    return -log_likeH(theta, campione)
+
+#Stima 0 (ipotizzo)
+theta0 = [1,1]
+
+res = minimize(neg_logL,theta0,args=(campione,),method='Nelder-Mead')
+
+print(res.x[0],res.x[1])
+
 
 #%%
-
 import scipy.stats as stats
+import numpy as np
+from scipy.optimize import minimize
 
-#La concentrazione di zuccheri in una bevanda non puo superare il 10%.
-#Quale istruzione e’ corretta per fare un test di ipotesi su 70 bevande per
-#la certificazione?
+#Generare un campione casuale di 100 elementi da una distribuzione normale con parametri μ=1.5
+# e σ=1.5
+#, utilizzando come seme generatore 100. Stimare con il metodo MLE i valori di μ
+# e σ
+#.
+#{ N.B. Utilizzare come valori iniziale μ0=1
+#, σ0=1
+# e come metodo per l’ottimizzazione method="Nelder-Mead"}.
 
-rvs = stats.uniform.rvs(size=70,random_state=10)
-tt = stats.ttest_1samp(rvs, popmean=0.1,alternative='greater')
-print(tt[1])
+np.random.seed(100)
+
+campione = np.random.normal(loc=1.5,scale=1.5,size=100)
+
+def log_likH(theta,campione):
+    mu,ds = theta
+    log = np.sum(stats.norm.logpdf(campione,loc=mu, scale=ds))
+    return log
+
+def neg_logLike1(theta,campione):
+    return -log_likH(theta, campione)
+
+
+#Valori iniz
+theta0 = [1,1]
+
+#res
+res = minimize(neg_logLike1,theta0,args=(campione,),method="Nelder-Mead")
+print(res.x[0])
+print(res.x[1])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
--- primiEx.py
+++ primiEx.py
@@ -109,7 +109,7 @@ print(probXtra25e26)
 
 #%%
 
-
+import scipy.stats as stats
 #Un modello di auto h`a una velocit`a massima con distribuzione normale
 #con media 180 km/h e deviazione standard 2 km/h. Eseguendo un test su
 #una macchina,:
@@ -187,6 +187,14 @@ print(f"Statistica t: {risultato.statistic}, p-value: {risultato.pvalue}")
 #t vicino a 0: Poca differenza rispetto a H0
 #t negativo: La media campionaria è inferiore a 0.1
 
+#alternative='greater'
+#Inende che deve essere al massimo H0. Ovvero Ha: mu>0.1
+
+#alternative='less'
+#La concentrazione di cemento di un edificio in costruzione deve essere di almeno il 74% -> H0: 0.74
+#Inende che deve essere al minimo H0. Ovvero Ha: mu<0.74
+
+#Se non c'è scritto nienete. Ha: mu != 0.1
 
 #%%
 
@@ -328,30 +336,143 @@ print(intervallo)
 
 #%%
 
-import scipy.stats as stats 
+import scipy.stats as stats
 import numpy as np
+from scipy.optimize import minimize
 
-#Generare un campione casuale di 20 elementi da una distribuzione binomiale con p=0.55, n=100, utilizzando come seme generatore 10. Stimare
-#con il metodo MLE il valore di p supponendo noto n.
+
+#Generare un campione casuale di 20 elementi da una distribuzione bino
+#miale con p=0.55, n=100, utilizzando come seme generatore 10. Stimare
+# con il metodo MLE il valore di p supponendo noto n.
 
 np.random.seed(10)
+
 campione = np.random.binomial(n=100, p=0.55,size=20)
 print(campione)
 
-#%%
-#Genera un campione casuale con 30 elementi da una distribuzione normale cone
-#media  mu = 1 e std = 1, tilizzando come seme genratore 10.
-#Stimare i valori di mu e std.
-std =1 #deviazion estandard
-mu = 1 #media 
-np.random.seed(10)
-campione = np.random.normal(mu,std,size=10)
+#trovo il log della L(tetha)
+def log_likeH(theta,campione):
+    n = 100
+    p = theta
+    log_like = np.sum(stats.binom.logpmf(campione,n= n, p=p))
+    return log_like
+
+#Faccio il -log(L(Thetha)) -> Mimimizzo 
+#Minimizzazione della fuznione di verosmiglianza
+
+def neg_log_like(theta,campione):
+    return -log_likeH(theta, campione)
+
+#non dato da esercizio (inventato)
+#Ipotesi iniziale
+theta0 = 0.6
+
+res = minimize(neg_log_like,theta0,args=(campione,),method='Nelder-Mead')
+
+
+print(res.x[0])
 
 #%%
-#Genera un campione casuale di 30 elementi da una distribuzione esponenziale.
-#Con lamda=12, utilizzando come seme generatore 10. Stimare il metodo MLE il valore di 
-#lamda
-campione = np.random.exponential(scale=1/12,size=30)
+import scipy.stats as stats
+import numpy as np
+from scipy.optimize import minimize
+
+
+#Generare un campione casuale di 30 elementi da una distribuzione normale
+# con media mu= 1 e deviazione standard std=1, utilizzando come seme
+# generatore 10. Stimare con il metodo MLE il valore di mu e std.
+
+np.random.seed(10)
+
+campione = np.random.normal(size=30,loc = 1,scale=1)
+
+def log_likeH(theta,campione):
+    mu,ds = theta
+    logL = np.sum(stats.norm.logpdf(campione,loc=mu,scale=ds))
+    return logL
+
+def neg_logL(theta,campione):
+    return -log_likeH(theta, campione)
+
+#Stima 0 (ipotizzo)
+theta0 = [1,1]
+
+res = minimize(neg_logL,theta0,args=(campione,),method='Nelder-Mead')
+
+print(res.x[0],res.x[1])
+
+
+#%%
+import scipy.stats as stats
+import numpy as np
+from scipy.optimize import minimize
+
+#Generare un campione casuale di 100 elementi da una distribuzione normale con parametri μ=1.5
+# e σ=1.5
+#, utilizzando come seme generatore 100. Stimare con il metodo MLE i valori di μ
+# e σ
+#.
+#{ N.B. Utilizzare come valori iniziale μ0=1
+#, σ0=1
+# e come metodo per l’ottimizzazione method="Nelder-Mead"}.
+
+np.random.seed(100)
+
+campione = np.random.normal(loc=1.5,scale=1.5,size=100)
+
+def log_likH(theta,campione):
+    mu,ds = theta
+    log = np.sum(stats.norm.logpdf(campione,loc=mu, scale=ds))
+    return log
+
+def neg_logLike1(theta,campione):
+    return -log_likH(theta, campione)
+
+
+#Valori iniz
+theta0 = [1,1]
+
+#res
+res = minimize(neg_logLike1,theta0,args=(campione,),method="Nelder-Mead")
+print(res.x[0])
+print(res.x[1])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
